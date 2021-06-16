b961c6300435e4252a87f955a37e56d754c61d2e
HelloGit2309 <2309akash@gmail.com>
2021-06-16 12:11:04 +0530

    fixed update error

--- updating.php
+++ updating.php
@@ -8,12 +8,10 @@ $email = mysqli_real_escape_string($con, $_POST['email']);
 $phone = mysqli_real_escape_string($con, $_POST['phone']);
 $add = mysqli_real_escape_string($con, $_POST['add']);
 $city = mysqli_real_escape_string($con, $_POST['city']);
-if(empty($email)) {	
 if(empty($email)) {
 echo '<font color="red">Email field is empty.</font><br>';	
-} 
 }else {	
-$result = mysqli_query($con, "update mst_user set email='$email', username='$name',phone='$phone',address='$add',city='$city' where login=$id");
+$result = mysqli_query($con, "update mst_user set email='$email', username='$name',phone='$phone',address='$add',city='$city' where login='$id'");
 echo "<font color='green' >Update Successful</font> <br/>";
 echo '<strong><a href="index.php">Return Home</a></strong>';
 }
